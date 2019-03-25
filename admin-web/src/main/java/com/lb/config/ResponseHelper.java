6fd541e07f86ef6b4874ed510efba19fe4976059
Carrokle <870333908@qq.com>
2019-03-25 14:14:53 +0800

    增加docker部署

---
+++ admin-web/src/main/java/com/lb/config/ResponseHelper.java
@@ -0,0 +1,46 @@
+package com.lb.config;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Date: 2019-2-28
+ * Description:统一返回响应参数
+ */
+public class ResponseHelper {
+    public ResponseHelper() {
+    }
+
+    public static <T> ResponseModel<T> notFound(String message){
+        ResponseModel response = new ResponseModel();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setCode(HttpStatus.NOT_FOUND.getReasonPhrase());
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T> ResponseModel<T> internalServerError(String message){
+        ResponseModel response = new ResponseModel();
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setCode(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T> ResponseModel<T> validationFailure(String message){
+        ResponseModel response = new ResponseModel();
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setCode(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T> ResponseModel<T> buildResponseModel(T result){
+        ResponseModel response = new ResponseModel();
+        response.setStatus(HttpStatus.OK.value());
+        response.setCode(HttpStatus.OK.getReasonPhrase());
+        response.setMessage(HttpStatus.OK.getReasonPhrase());
+        response.setResult(result);
+        return response;
+    }
+}
+
